2e7875099d046d5d4dc4c72ec008cda26708d9d1
josep233 <113069167+josep233@users.noreply.github.com>
2023-02-02 10:52:05 -0700

    Update hw2_1.py

--- hw2_1.py
+++ hw2_1.py
@@ -5,7 +5,7 @@
 #import needed libraries
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+# from scipy.optimize import minimize
 import numdifftools as nd
 
 #==================================================================================================================================================================================================================================================================================
@@ -17,13 +17,16 @@ def f(x):
 # def f(x):
 #     return (1-x[0])**2 + (1-x[1])**2 + 0.5 * (2*x[1]-x[0]**2)**2
 def fp(x):
+    b = 0
+    # grad1 = 2*x[0] + b * x[1]
+    # grad2 = 2*x[1] + b * x[0]
+    # grad = np.array([grad1,grad2])
     grad = nd.Gradient(fun=f)(x)
     return grad
 #==================================================================================================================================================================================================================================================================================
 def phi(x,alpha,p):
     return f(x + alpha * p)
 def phip(x,alpha,p):
-    
     return np.dot(fp(x + alpha * p),p)
 #==================================================================================================================================================================================================================================================================================
 def interp(phiplow,phiphigh,philow,phihigh,alphalow,alphahigh):
@@ -34,15 +37,10 @@ def interp(phiplow,phiphigh,philow,phihigh,alphalow,alphahigh):
 #==================================================================================================================================================================================================================================================================================
 def pinpoint(x0,p,alphalow,alphahigh,mu1,mu2,phi0,phip0,philow,phiplow,phihigh,phiphigh):
     k = 0
-    j = 0
     while True:
         alphap = interp(phiplow,phiphigh,philow,phihigh,alphalow,alphahigh)
         phi_p = phi(x0,alphap,p)
         if (phi_p > (phi0 + mu1 * alphap * phip0)) or (phi_p > philow):
-            j = j + 1
-            if j > 3:
-                print(alphap)
-                return alphap
             alphahigh = alphap
             phihigh = phi_p
         else:
@@ -82,8 +80,6 @@ def bracketing(alphainit,phi0,phip0,mu1,mu2,sigma,p):
 def steepest(tau,x0,alpha0,mu1,mu2,sigma):
     k = 0
     x = x0.copy()
-    xk = x0.copy()
-    fk = f(xk)
     nfp = 1
     alpha = alpha0
     while nfp > tau:
@@ -97,7 +93,7 @@ def steepest(tau,x0,alpha0,mu1,mu2,sigma):
         x = x0 + alpha * p
         k = k + 1
         nfp = np.linalg.norm(fp(x))
-    return xk, fk
+    return x
 
 def alpha_estimate(p,x,x0,p0,alpha0):
     alpha = alpha0 * (np.dot(fp(x0),p0) / np.dot(fp(x),p))
@@ -111,20 +107,9 @@ mu2 = 0.9
 sigma = 2
 tau = 1E-6
 
-xk,fk = steepest(tau,x0,alpha0,mu1,mu2,sigma)
-print(fk)
+x = steepest(tau,x0,alpha0,mu1,mu2,sigma)
+print(x)
 #==================================================================================================================================================================================================================================================================================
-# alpha0 = 0.3
-# x0 = np.array([-7.5,2.5])
-# mu1 = 0.1
-# mu2 = 0.2
-# sigma = 2
-# tau = 1E-6
-
-# xk,fk = steepest(tau,x0)
-
-# print(xk,fk)
-
 def fun(x1,x2):
     fun = np.zeros([len(x1),len(x2)])
     for i in range(len(x1)):

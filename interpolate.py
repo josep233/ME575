78bb1a7fa0d2b54aef60ca5dae4d52139edfa100
Joseph <joeyscarter@gmail.com>
2023-02-03 18:27:20 -0700

    N/A

--- interpolate.py
+++ interpolate.py
@@ -5,11 +5,17 @@ import funs as f
 def interpolate(x,alpha1,alpha2,p):
     if abs(alpha1 - alpha2) > 1E-10:
         beta1 = f.phip(x,alpha1,p) + f.phip(x,alpha2,p) - 3 * ((f.phi(x,alpha1,p) - f.phi(x,alpha2,p))/(alpha1 - alpha2))
-        beta2 = np.sign(alpha2 - alpha1) * np.sqrt(beta1**2 - f.phip(x,alpha1,p) * f.phip(x,alpha2,p))
-        alpha_s = alpha2 - (alpha2 - alpha1) * ((f.phip(x,alpha2,p) + beta2 - beta1)/(f.phip(x,alpha2,p) - f.phip(x,alpha1,p) + 2 * beta2))
+        if beta1**2 - f.phip(x,alpha1,p) * f.phip(x,alpha2,p) > 0:
+            beta2 = np.sign(alpha2 - alpha1) * np.sqrt(beta1**2 - f.phip(x,alpha1,p) * f.phip(x,alpha2,p))
+            alpha_s = alpha2 - (alpha2 - alpha1) * ((f.phip(x,alpha2,p) + beta2 - beta1)/(f.phip(x,alpha2,p) - f.phip(x,alpha1,p) + 2 * beta2))
+            print(alpha_s)
+            return alpha_s
+        else:
+            alpha_s = (alpha1 + alpha2) / 2
+            return alpha_s
     else:
         alpha_s = (alpha1 + alpha2) / 2
-    return alpha_s
+        return alpha_s
 
 # x = np.array([3.5,5])
 # p = - fp(x) / np.linalg.norm(fp(x))

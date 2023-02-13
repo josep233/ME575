5d4d701610e30cdbdd175b7424960952f5079570
Joseph <joeyscarter@gmail.com>
2023-02-13 10:56:46 -0700

    N/A

--- project/ObjectiveFunction.py
+++ project/ObjectiveFunction.py
@@ -7,9 +7,9 @@ def ObjectiveFunction(stalk):
     pithprops = p.properties(stalk)[0]
     rindprops = p.properties(stalk)[1]
     a.script(stalk,pithprops,rindprops)
-    ans = subprocess.run("abaqus cae noGUI=C:\\Users\\Joseph\\Desktop\\Temp\\test.py", capture_output=True,text=True)
-    print(ans)
+    ans = subprocess.Popen("abaqus cae noGUI=C:\\Users\\Joseph\\Desktop\\Temp\\test.py",stdout=subprocess.PIPE,shell=True)
+    print(ans.communicate())
 
-
+ 
 stalk = 7
 ObjectiveFunction(stalk)
